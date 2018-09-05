81fb16dcce8d9ec8233efd375eec53104bd00e06
rolotorres <rolotorres192@gmail.com>
2018-09-04 21:17:54 -0400

    correción en despliege de mapa y mejora en el tamaño de vista

--- src/app/shared/mapa/mapa.component.ts
+++ src/app/shared/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Marcador } from '../../classes/marcador.class';
 
 @Component({
   selector: 'app-mapa',
@@ -7,12 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaComponent implements OnInit {
 
+  marcadores: Marcador[] = [];
+
   lat: number = -25.2968361;
   lng: number = -57.6681294;
-  
-  constructor() { }
+
+  constructor() {
+    if(localStorage.getItem('marcadores')){
+      this.marcadores = JSON.parse(localStorage.getItem('marcadores'));
+    }
+  }
+
 
   ngOnInit() {
+  }
+
+  agregarMarcador(evento) {
+    const coords: { lat: number, lng: number } = evento.coords;
+
+    const nuevoMarcador = new Marcador(coords.lat, coords.lng);
+
+    this.marcadores.push(nuevoMarcador);
+
+    this.guardarStorage();
+
+  }
+
+  guardarStorage() {
+    localStorage.setItem('marcadores', JSON.stringify(this.marcadores));
+  }
+
+  borrarMarcador(i: number){
+    this.marcadores.splice(i, 1);
+
+    this.guardarStorage();
+  }
+
+  editarMarcador(){
+
   }
 
 }
